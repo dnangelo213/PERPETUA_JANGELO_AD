23bca0f1a5b415ac748edbfac8323ab339bec9f6
Angelo Perpetua <angeloperpetuaaa@gmail.com>
2021-09-08 22:37:16 +0800

    Portal and Javascript

---
+++ 2_JS/LoopsTriangle.js
@@ -0,0 +1,6 @@
+var height = 5
+height = "*";
+
+for (let height = 1; height < 6; height++); {
+    console.log(height)
+}
---
+++ 2_JS/VariableType.js
@@ -0,0 +1,18 @@
+var number = 1;
+var a = [1, 2, 3, 4, 5, 6];
+var b = { one: 1, "two": "two", three: [3] };
+var z
+var n = null
+console.log(a);
+console.log(`The variable of 1,2,3,4,5,6 is of type `, typeof a);
+console.log(b);
+console.log(`The variable ${b} is of type`, typeof b);
+console.log(z);
+console.log(`The variable ${z} is of type`, typeof z);
+console.log(n);
+console.log(`The variable ${n} is of type`, typeof n);
+console.log(number);
+console.log(`The variable ${number} is a type of`, typeof number);
+var e = ("HELLO I AM A STRING");
+console.log(e);
+console.log(`The variable ${e} is of type`, typeof e);
