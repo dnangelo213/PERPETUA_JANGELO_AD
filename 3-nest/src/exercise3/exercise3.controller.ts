109e17167108f915537f7f1dc8dea95dbe71d3a3
Angelo Perpetua <angeloperpetuaaa@gmail.com>
2021-09-22 21:06:17 +0800

    Making first API

--- 3-nest/src/exercise3/exercise3.controller.ts
+++ 3-nest/src/exercise3/exercise3.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Exercise3Service } from './exercise3.service';
 
 @Controller('exercise3')
-export class Exercise3Controller {}
+export class Exercise3Controller {
+    constructor(private readonly e3: Exercise3Service) {}
+
+    @Get('/Helloworld/:name')
+    getHello(@Param('name')name:string): string {
+      return this.e3.helloWorld(name);
+    }
+
+    @Get('/LoopsTriangle/:height')
+    loopsTriangle(@Param('id') height:string ){
+        var parsedheight = parseInt(height);
+      return this.e3.loopsTriangle(parsedheight , "");
+    }   
+
+    @Get('/PrimeNumber/:num')
+    primeNumber(@Param('num') num:number){
+        return this.e3.primenumber(num);
+    }
+}
--- 3-nest/src/exercise3/exercise3.service.ts
+++ 3-nest/src/exercise3/exercise3.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class Exercise3Service {}
+export class Exercise3Service {
+    helloWorld(name:string){   
+        return "Hello there " + name;
+    }
+    loopsTriangle(height:number, string:""){
+        for (var i = 0; i < height; i++) {
+            for (var a = 0; a < i; a++) {
+                string += "* ";
+            }
+            string += "\n";
+        }
+        console.log(string);
+    }
+    return ;
+
+    primenumber(num:number){
+        let isPrime = true;
+
+    if (num === 1) {
+        console.log("1 is neither prime nor composite number.");
+         } else if (num > 1) {
+
+     for (let i = 2; i < num; i++) {
+         if (num % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log('$ {num} is a prime number');
+        return num + " is a prime number";
+    } else {
+        console.log('$ {num} is not prime number');
+        return num + " is not prime number";
+    }
+}
+    }
+}
